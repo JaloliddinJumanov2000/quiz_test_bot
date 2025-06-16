7480896c53ee7bc9e5d201cc6c8f981ebcfd5dca
Jaloliddin Jumanov <jumanovjaloliddin7@gmail.com>
2025-06-16 20:33:38 +0500

    new

--- utils/db/database.py
+++ utils/db/database.py
@@ -1,10 +1,14 @@
 from sqlalchemy import create_engine, Column, String, Integer, BigInteger
 from sqlalchemy.orm import declarative_base, sessionmaker
+from sqlalchemy.testing.suite.test_reflection import users
+
 from data.config import PG_PASS,PG_USER,PG_HOST,PG_PORT,PG_DB
 
 engine = create_engine(f'postgresql+psycopg2://{PG_USER}:{PG_PASS}@{PG_HOST}:{PG_PORT}/{PG_DB}', echo=True)
 
 Base = declarative_base()
+Session = sessionmaker(bind=engine)
+session = Session()
 
 class User(Base):
     __tablename__ = 'users'
@@ -12,3 +16,12 @@ class User(Base):
     chat_id = Column(BigInteger,unique=True)
     fullname = Column(String(50), nullable=False)
     phone = Column(String(20), nullable=False)
+
+if __name__ == '__main__':
+    user=User(chat_id=5937680085,fullname='Jaloliddin Jumanov',phone='+998915622608')
+    session.add(user)
+    # user=session.query(User).filter(User.chat_id ==5937680085).first()
+    # if user:
+    #     # user.fullname='Jumanov Jaloliddin'
+    #     session.delete(user)
+    session.commit()
